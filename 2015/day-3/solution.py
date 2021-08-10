b4b42878a47869942a35df3df791d521c1d26e81
Nano-AI <adi.bankoti@gmail.com>
2021-08-10 10:48:48 -0700

    Working on day 3

---
+++ 2015/day-3/solution.py
@@ -0,0 +1,35 @@
+from math import floor
+
+f = open('input.txt')
+inp = f.read()
+
+index_data = {
+    '^': (0, 1),
+    'v': (0, -1),
+    '>': (1, 0),
+    '<': (-1, 0),
+}
+
+def part_1():
+    houses = 0
+    x, y = 0, 0
+    visited = []
+    for direction in inp:
+        if (x, y) not in visited:
+            houses += 1
+            visited.append((x, y))
+        dx, dy = index_data[direction]
+        x += dx
+        y += dy
+    return houses
+
+def part_2():
+    houses = 0
+    santa_x, santa_y = 0, 0
+    robot_x, robot_y = 0, 0
+    for i, direction in enumerate(inp):
+        print(direction)
+    return houses
+
+print(part_1())
+print(part_2())
